d14574fa6dd38324b9e6a39cc38cdb45fac790d9
M N Karthik <malayanurkarthik0@gmail.com>
2026-04-22 09:29:26 +0530

    express_app

---
+++ containerise_express_app/src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+const users = [];
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+app.post('/users', (req, res) => {
+    const newUserID =  req.body.userId;
+    if (!newUserID) {
+        return res.status(400).json({ error: 'userId is required' });
+    }
+    if (users.includes(newUserID)) {
+        return res.status(400).json({ error: 'User already exists' });
+    }
+    users.push(newUserID);
+    res.status(201).json({ message: 'User added successfully', userId: newUserID });
+});
+
+app.listen(port, () => {
+  console.log(`Express app listening at ${port}`);
+});
